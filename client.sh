04567764f073547987cb85cd97a0ddcf54b16933
siniida <sinpukyu@gmail.com>
2015-07-23 17:58:36 +0900

    別名対応

--- client.sh
+++ client.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -ti --rm --link gennaidocker_zookeeper_1:zk --link gennaidocker_gungnir_1:gungnir --link gennaidocker_tuplestore_1:tuplestore siniida/gungnir-client
+PWD=`pwd`
+BASE=`basename ${PWD} | sed -e 's/[\.\-]//g'`
+
+docker run -ti --rm --link ${BASE}_zookeeper_1:zk --link ${BASE}_gungnir_1:gungnir --link ${BASE}_tuplestore_1:tuplestore siniida/gungnir-client
