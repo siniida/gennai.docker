68c0cf4bb5036b6cf3c68000112da8ab6afaae31
siniida <sinpukyu@gmail.com>
2015-10-09 12:24:28 +0900

    delete deprecated container.

--- rebuild.sh
+++ rebuild.sh
@@ -2,8 +2,6 @@
 
 docker build -t siniida/zookeeper zookeeper
 docker build -t siniida/kafka kafka
-docker build -t siniida/storm-nimbus storm-nimbus
-docker build -t siniida/storm-supervisor storm-supervisor
+docker build -t siniida/storm storm
 docker build -t siniida/gungnir-server gungnir-server
-docker build -t siniida/tuple-store-server tuple-store-server
 docker build -t siniida/gungnir-client gungnir-client
