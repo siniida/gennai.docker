8e5402b9c908d4243d40fc54b215c6303e7aeb46
siniida <sinpukyu@gmail.com>
2015-10-30 20:21:52 +0900

    bug fix.

--- client.sh
+++ client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PWD=`pwd`
-BASE=`basename ${PWD} | sed -e 's/[\.\-]//g'`
+BASE=`basename ${PWD} | sed -e 's/[\.\-\_]//g'`
 
 docker run -ti --rm --link ${BASE}_zookeeper_1:zk --link ${BASE}_gungnir_1:gungnir --link ${BASE}_tuplestore_1:tuplestore siniida/gungnir-client
