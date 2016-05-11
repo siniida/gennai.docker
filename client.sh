868e29f679ab116922101f38161cc3a25f5c8c61
siniida <sinpukyu@gmail.com>
2016-05-11 11:47:56 +0900

    use alpine linux.

--- client.sh
+++ client.sh
@@ -3,4 +3,4 @@
 PWD=`pwd`
 BASE=`basename ${PWD} | sed -e 's/[\.\-\_]//g'`
 
-docker run -ti --rm --link ${BASE}_zookeeper_1:zk --link ${BASE}_gungnir_1:gungnir --link ${BASE}_tuplestore_1:tuplestore siniida/gungnir-client
+docker run -ti --rm --link ${BASE}_zookeeper_1:zk --link ${BASE}_gungnir_1:gungnir --link ${BASE}_tuplestore_1:tuplestore siniida/gungnir-client:0.0.1-alpine
